b8bcbe3adc902dc0bb89cfc647de442b96c0d1fc
komangbayu99 <55182023+komangbayu99@users.noreply.github.com>
2019-11-20 14:53:49 +0700

    Add files via upload

---
+++ upload-file/index_upload.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PedoS | Upload File</title>
+</head>
+<body>
+	<h1>Upload File<br/></h1>
+	<form action="aksi_upload.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="file">
+		<input type="submit" name="upload" value="upload">
+	</form>
+</body>
+</html>
